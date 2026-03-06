1c5fa705cc73917754ac18255b19d5ed5db5771c
Antonio Monzo <AMONANSU@GMAIL.COM>
2026-03-06 12:12:22 +0100

    feat(Button):  Ejempllo de elemento button

---
+++ src/Input.jsx
@@ -0,0 +1,10 @@
+function Input () {
+    return (
+        <div>
+            <input type="text" name="" id="" />
+            <button>Click me</button>
+
+        </div>
+    );
+}
+export default Input;
